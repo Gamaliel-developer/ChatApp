7addb79a755706c2d565b8215c3b9c9adff4c6d8
Gamaliel-developer <galeonlp05@gmail.com>
2023-02-16 20:01:41 -0700

    practica

---
+++ app.config.js
@@ -0,0 +1,50 @@
+import 'dotenv/config';
+
+export default
+{
+  "expo": {
+    "extra": {
+      "eas": {
+        "projectId": "88199112-91e0-44dd-bb48-63fa5cb72ce7"
+      }
+    },
+    "name": "ChatApp",
+    "slug": "ChatApp",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#FFFFFF"
+      },
+      "package": "com.galeon.ChatApp"
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
+  }
+}
